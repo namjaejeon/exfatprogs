hclee/exfat-tools

fsck: add function to get an FAT entry
Add get_next_clus to get an FAT entry.

Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

fsck/fsck.c
@@ -163,7 +163,7 @@ static int check_clus_chain(struct exfat_de_iter *de_iter,
 		}
 
 		/* This cluster is allocated or not */
-		if (get_next_clus(exfat, node, clus, &next))
+		if (get_inode_next_clus(exfat, node, clus, &next))
 			goto truncate_file;
 		if (!node->is_contiguous) {
 			if (!heap_clus(exfat, next) &&
@@ -243,7 +243,7 @@ static bool root_get_clus_count(struct exfat *exfat, struct exfat_inode *node,
 
 		exfat_bitmap_set(exfat->alloc_bitmap, clus);
 
-		if (get_next_clus(exfat, node, clus, &clus) != 0) {
+		if (get_inode_next_clus(exfat, node, clus, &clus) != 0) {
 			exfat_err("/: broken cluster chain\n");
 			return false;
 		}

namjaejeon/exfatprogs

fsck/fsck.c
@@ -159,7 +159,7 @@ static int check_clus_chain(struct exfat_de_iter *de_iter,
 		}
 
 		/* This cluster is allocated or not */
-		if (exfat_get_next_clus(exfat, node, clus, &next))
+		if (exfat_get_inode_next_clus(exfat, node, clus, &next))
 			goto truncate_file;
 		if (next == EXFAT_BAD_CLUSTER) {
 			if (repair_file_ask(de_iter, node,
@@ -253,7 +253,7 @@ static bool root_get_clus_count(struct exfat *exfat, struct exfat_inode *node,
 
 		exfat_bitmap_set(exfat->alloc_bitmap, clus);
 
-		if (exfat_get_next_clus(exfat, node, clus, &clus) != 0) {
+		if (exfat_get_inode_next_clus(exfat, node, clus, &clus) != 0) {
 			exfat_err("/: broken cluster chain\n");
 			return false;
 		}